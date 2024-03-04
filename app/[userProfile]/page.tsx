e6fe81733479c18bb0bf8c50032bebff50719c22
rahul@clearcut.design <rahul@clearcut.design>
2024-03-04 13:20:16 +0530

    basic setup

--- app/[userProfile]/page.tsx
+++
@@ -1,5 +0,0 @@
-import { createClient } from "@/utils/supabase/client";
-
-export default async function ProfilePage() {
-  return <div>Hello</div>;
-}
---
+++ app/profile/page.tsx
@@ -0,0 +1,5 @@
+import { createClient } from "@/utils/supabase/client";
+
+export default async function Profile() {
+  return <div>USER PROFILE</div>;
+}
