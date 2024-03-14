d2ccfa00f46dd537c61a76caacf977c3356f138f
rahul@clearcut.design <rahul@clearcut.design>
2024-03-15 05:23:05 +0530

    metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,9 +8,10 @@ const defaultUrl = process.env.VERCEL_URL
   : "http://localhost:3000";
 
 export const metadata = {
-  metadataBase: new URL(defaultUrl),
-  title: "Next.js and Supabase Starter Kit",
-  description: "The fastest way to build apps with Next.js and Supabase",
+  metadataBase: "https://pawmingle.com", // Your site's base URL
+  title: "Paw Mingle - Pet Adoption Made Easy",
+  description:
+    "Join Paw Mingle, a loving community dedicated to making pet adoption seamless. Find your forever friend today!",
 };
 
 export default function RootLayout({
