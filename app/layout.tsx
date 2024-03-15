9ef4e98e80cb6f1419bfc733874ef0409b649ac2
rahul@clearcut.design <rahul@clearcut.design>
2024-03-16 03:10:53 +0530

    Navbar changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({
     <html lang="en" className={GeistSans.className}>
       <body className="bg-background text-foreground">
         <main className="min-h-screen flex flex-col items-center">
-          <Navbar links={[{ href: "/", label: "PawMingle🐾" }]} className="">
+          <Navbar className="">
             <AuthButton />
           </Navbar>
           {children}
--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,32 +1,45 @@
-import React, { ReactNode } from "react";
-import Link from "next/link";
+import React from "react";
+import Link from "next/link"; // Import Link from Next.js
 
+// Logo component for Pawmingle
+const Logo = () => (
+  <div className="flex items-center">
+    <span className=" hover:shadow-xl font-black text-2xl transition-colors duration-150 ease-in-out">
+      PawMingle 🐾
+    </span>
+  </div>
+);
+
+// NavbarLink interface tailored for Next.js usage
 interface NavbarLink {
   href: string;
   label: string;
-  iconName?: ReactNode;
+  iconName?: React.ReactNode;
   className?: string;
 }
 
 interface NavbarProps {
   links?: NavbarLink[];
-  children?: ReactNode;
+  children?: React.ReactNode;
   className?: string;
 }
 
 const Navbar: React.FC<NavbarProps> = ({ links, children, className }) => {
   return (
     <nav
-      className={`w-full flex justify-center bg-black border-b border-gray-200 shadow-lg h-20 sticky top-0 z-50 ${className}`}
+      className={`w-full bg-black border-b border-gray-200 shadow-lg sticky top-0 z-50 ${className}`}
     >
-      <div className="w-full max-w-6xl flex justify-between items-center px-4 sm:px-6 lg:px-8">
-        <div className="flex items-center space-x-4 md:space-x-8">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 h-20 flex justify-between items-center">
+        <Link href="/" passHref className="flex items-center justify-center">
+          <Logo />
+        </Link>
+        <div className="flex-1 flex items-center justify-center space-x-4 md:space-x-8">
           {links?.map((link, index) => (
             <Link
               key={index}
               href={link.href}
               passHref
-              className={`flex items-center font-semibold text-lg transition duration-150 ease-in-out transform hover:scale-105 ${link.className}`}
+              className={`text-gray-300 hover:text-white transition-colors duration-150 ease-in-out ${link.className}`}
             >
               {link.iconName && <span className="mr-2">{link.iconName}</span>}
               {link.label}
