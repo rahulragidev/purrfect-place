16c8e928226999128d5bd006f32e46082ca9ecbb
rahul@clearcut.design <rahul@clearcut.design>
2024-03-21 04:28:13 +0530

    minor changes

--- types/pet.ts
+++ types/pet.ts
@@ -5,12 +5,12 @@ export interface Pet {
   breed: string | null;
   age: number | null; // integer
   description: string | null;
-  status: 'available' | 'adopted' | null;
+  status: 'available' | 'adopted'
   provider_user_id: string; // UUID
   adopter_user_id: string | null; // UUID, can be null
   latitude: number | null; // numeric(9,6)
   longitude: number | null; // numeric(9,6)
   created_at: Date; // timestamp with time zone
   updated_at: Date | null; // timestamp with time zone
-  photos: string[] | null; // array of text, representing URLs to photos
+  photos: string[] // array of text, representing URLs to photos
 }
