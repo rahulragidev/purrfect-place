aae3bc5c9705921efeba6c39a37cf286180d28c4
rahul@clearcut.design <rahul@clearcut.design>
2024-03-21 07:52:30 +0530

    link update

--- app/pets/[petId]/page.tsx
+++ app/pets/[petId]/page.tsx
@@ -20,7 +20,7 @@ export default async function Page({ params }: PageParams) {
           <div className="flex justify-between items-center mb-4">
             <h1 className="text-3xl font-semibold">{petDetails[0].name}</h1>
             <Link
-              href="/login"
+              href={`/messages/${params.petId}`}
               className="inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-green-600 hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500"
             >
               Adopt {petDetails[0].name} <span className="ml-2">&#x1f43e;</span>
