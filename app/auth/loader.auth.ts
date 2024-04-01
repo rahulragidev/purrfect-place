41a7ca94fda3dbe49aa53b305301224a0a2e4e54
rahul@clearcut.design <rahul@clearcut.design>
2024-04-01 20:48:12 +0700

    redirect the user to logIn if not loggedIn

--- app/auth/loader.auth.ts
+++ app/auth/loader.auth.ts
@@ -1,14 +1,28 @@
 import { createClient } from "@/utils/supabase/server";
+import { redirect } from "next/navigation";
 
-  export const userId = async (): Promise<string> => {
-    "use server";
-    const supabase = createClient();
+export const userId = async (): Promise<string> => {
+  "use server";
+  const supabase = createClient();
+
+  try {
     const { data, error } = await supabase.auth.getUser();
+
     if (error) {
-      throw new Error("Error fetching user: " + error.message);
+      if (error.message.includes("Invalid Refresh Token")) {
+        redirect("/login");
+      } else {
+        throw new Error("Error fetching user: " + error.message);
+      }
     }
+
     if (!data.user || !data.user.id) {
       throw new Error("User ID is undefined");
     }
+
     return data.user.id;
-  };
+  } catch (error) {
+    console.error("Error in userId function:", error);
+    redirect("/login");
+  }
+};
