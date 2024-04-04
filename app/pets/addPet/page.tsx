66eef48b9f119c1cd7161635d7d269d96c8e5e46
rahul@clearcut.design <rahul@clearcut.design>
2024-04-04 20:51:35 +0700

    foraction changed to submit button

--- app/pets/addPet/page.tsx
+++ app/pets/addPet/page.tsx
@@ -88,7 +88,7 @@ export default function AddPet({
             Fill in the details below to add a pet for adoption.
           </p>
         </div>
-        <form className="mt-8 space-y-6" action={addPet}>
+        <form className="mt-8 space-y-6">
           <div className="rounded-md shadow-sm -space-y-px">
             <div className="">
               <label htmlFor="name" className="sr-only">
@@ -199,7 +199,10 @@ export default function AddPet({
           </div>
 
           <div>
-            <SubmitButton className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">
+            <SubmitButton
+              formAction={addPet}
+              className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
+            >
               Add Pet
             </SubmitButton>
           </div>
