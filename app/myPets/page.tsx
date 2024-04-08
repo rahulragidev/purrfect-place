f2425a7c686661b82070949d8ed62607e11c092e
rahul@clearcut.design <rahul@clearcut.design>
2024-04-09 03:49:17 +0700

    myPets design got changed

--- app/myPets/page.tsx
+++ app/myPets/page.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Link from "next/link";
 import PetCard from "@/components/PetCard";
 import { Pet } from "@/types/pet";
 import { fetchCurrentUserData } from "@/utils/users/fetchCurrentUserData";
@@ -10,11 +11,13 @@ export default async function PetsPage() {
   const pets = await fetchUserPets(user_id);
 
   return (
-    <div>
+    <div className="w-full px-4 py-12">
       <h1 className="flex font-black text-gray-200 text-3xl p-6">My Pets</h1>
-      <div className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 w-full pt-6">
+      <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
         {pets.map((pet: Pet) => (
-          <PetCard key={pet.pet_id} pet={pet} />
+          <Link href={`/pets/${pet.pet_id}`} key={pet.pet_id}>
+            <PetCard pet={pet} />
+          </Link>
         ))}
       </div>
     </div>
